f6006d831d09556bebbc0eabdb90c3f97501fdba
Salmon-ThinkPad <simon.patino@udea.edu.co>
2025-11-20 17:26:59 -0500

    Create - Read - Update - Delete: CRUM Implementation

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from collections.abc import AsyncIterator, Sequence
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI, HTTPException, status
-from sqlmodel import Field, Session, SQLModel, create_engine, select
+from sqlmodel import Field, Session, SQLModel, create_engine, select, text
 
 from config import settings
 
@@ -56,9 +56,51 @@ def health_check() -> dict:
     with Session(engine) as session:
         test = session.exec(select(1)).first()
 
+        version = session.exec(text("SELECT VERSION();")).scalar_one()
+
+        quantity = session.exec(text("SELECT COUNT(*) FROM hero;")).scalar_one()
+
         if test == 1:
-            return {"status": "healthy"}
+            return {"status": "healthyy", "version": version, "hero_count": quantity}
         else:
             raise HTTPException(
                 status_code=status.HTTP_503_SERVICE_UNAVAILABLE, detail="unhealthy"
             )
+
+
+@app.get("/heroes/{hero_id}")
+def read_hero(hero_id: int) -> Hero:
+    with Session(engine) as session:
+        hero = session.get(Hero, hero_id)
+        if not hero:
+            raise HTTPException(404)
+        return hero
+
+
+@app.put("/heroes/{hero_id}")
+def update_hero(hero_id: int, updated_hero: Hero) -> Hero:
+    with Session(engine) as session:
+        hero = session.get(Hero, hero_id)
+        if not hero:
+            raise HTTPException(404)
+        hero.name = updated_hero.name
+        hero.secret_name = updated_hero.secret_name
+        hero.age = updated_hero.age
+        session.add(hero)
+        session.commit()
+        session.refresh(hero)
+        return hero
+
+
+@app.delete("/heroes/{hero_id}")
+def delete_hero(hero_id: int) -> dict:
+    with Session(engine) as session:
+        hero = session.get(Hero, hero_id)
+
+        if not hero:
+            raise HTTPException(404)
+
+        hero_name = hero.name
+        session.exec(text(f"DELETE FROM hero WHERE id = {hero_id}"))
+        session.commit()
+        return {"status": f"deleted {hero_name}"}
